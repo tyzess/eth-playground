60e83ff1aa8e91584b61ec282a8845a976b19c98
stoney1989 <nico.hartma@gmail.com>
2017-11-08 09:37:54 +0100

    add simple bank contract

---
+++ dapp/playground/app/contracts/bank.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.7;
+
+
+contract Bank {
+
+    mapping (address => uint256) deposits;
+
+    function deposit() public payable {
+        deposits[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        msg.sender.send(deposits[msg.sender]);
+    }
+
+}
