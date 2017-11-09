a491f2e8c12a330f6d73a36ba049c0ec8a7a7219
stoney1989 <nico.hartma@gmail.com>
2017-11-09 09:55:19 +0100

    implement contract

---
+++ dapp/playground/app/contracts/tictactoetest.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.7;
+
+
+contract TicTacToe {
+
+    address[] players = [0x0, 0x0];
+
+    uint8[][] board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+    uint8 currentTurn = 0;
+
+    uint8 playerCount = 0;
+
+    uint8 MAX_PLAYERS = 2;
+
+    uint256 price = 100;
+
+    uint8 FIELD_SIZE = 3;
+
+    address owner;
+
+    function TicTacToe(){
+        owner = msg.sender;
+    }
+
+}
+
