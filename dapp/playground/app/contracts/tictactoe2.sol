427249dab6137e416f850d90a0cb46846e5d7e0c
stoney1989 <nico.hartma@gmail.com>
2017-11-10 10:43:12 +0100

    tictactoe simple

---
+++ dapp/playground/app/contracts/tictactoe2.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.0;
+
+
+contract tictactoe2 {
+
+    enum Token {NONE, X, O}
+    Token currentToken = Token.NONE;
+
+    mapping (uint8 => mapping (uint8 => Token)) board;
+
+
+    function tictactoe2() public {
+        resetGame();
+    }
+
+    function resetGame() public {
+        currentToken = Token.NONE;
+        for (uint x = 0; x < board.length; x++) {
+            for (uint y = 0; y < board.length; y++) {
+                board[x][y] = Token.NONE;
+            }
+        }
+    }
+
+}
