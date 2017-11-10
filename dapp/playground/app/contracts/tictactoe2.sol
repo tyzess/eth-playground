52dfd6d901ef550e01ff6f8b54302dcff65efaa4
stoney1989 <nico.hartma@gmail.com>
2017-11-10 11:21:10 +0100

    tictactoe simple

--- dapp/playground/app/contracts/tictactoe2.sol
+++ dapp/playground/app/contracts/tictactoe2.sol
@@ -3,12 +3,28 @@ pragma solidity ^0.4.0;
 
 contract TicTacToe2 {
 
-    uint8 BOARD_SIZE = 3;
+    uint8 public BOARD_SIZE = 3;
+
+    uint8 public MAX_PLAYERS = 2;
+
+    uint public playerCount = 0;
 
     enum Token {NONE, X, O}
-    Token currentToken = Token.NONE;
+    Token public currentToken = Token.NONE;
 
     mapping (uint8 => mapping (uint8 => Token)) board;
+
+    mapping (uint => address) tokenToPlayer;
+
+    function getPlayer1() public returns (address){
+        uint index = Token.X;
+        return tokenToPlayer[index];
+    }
+
+    function getPlayer2() public returns (address){
+        uint index = Token.O;
+        return tokenToPlayer[index];
+    }
 
 
     function TicTacToe2() public {
@@ -22,6 +38,48 @@ contract TicTacToe2 {
                 board[x][y] = Token.NONE;
             }
         }
+    }
+
+    function join() public payable returns (bool){
+        if (msg.value != price || allPlayersJoined()) {
+            return false;
+        }
+
+        playerCount++;
+        tokenToPlayer[playerCount] = msg.sender;
+
+        if (allPlayersJoined()) {
+            currentToken = Token.X;
+        }
+
+        return true;
+    }
+
+    function allPlayersJoined() public view returns (bool) {
+        return playerCount == MAX_PLAYERS;
+    }
+
+    function setToken(uint8 x, uint8 y) public returns (bool) {
+
+        if (!isInBounds(x) || !isInBounds(y)) {
+            return false;
+        }
+
+        if (isAlreadySet(x, y)) {
+            return false;
+        }
+
+        board[x][y] = currentTurn;
+
+        if (playerWon()) {
+            payOutWinner(getCurrentPlayer());
+            resetGame();
+        }
+        else {
+            nextTurn();
+        }
+
+        return true;
     }
 
 }
