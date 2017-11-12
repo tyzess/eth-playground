3508b40fd480575143135ed37f48e204956ce164
stoney1989 <nico.hartma@gmail.com>
2017-11-12 16:28:30 +0100

    tictactoe fix warnings & rewrite code

--- dapp/playground_truffle/migrations/1_initial_migration.js
+++ dapp/playground_truffle/migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var TicTacToe2 = artifacts.require("TicTacToe2.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+    deployer.deploy(TicTacToe2);
 };
